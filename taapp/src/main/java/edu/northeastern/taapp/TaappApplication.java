8cb10f77eb6c1c8c553d4f9b3af324872c747c35
Shobith Sivadas <sivadas.s@northeastern.edu>
2023-11-30 15:26:41 -0500

    Project created

---
+++ taapp/src/main/java/edu/northeastern/taapp/TaappApplication.java
@@ -0,0 +1,13 @@
+package edu.northeastern.taapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaappApplication.class, args);
+	}
+
+}
---
+++ taapp/src/test/java/edu/northeastern/taapp/TaappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.northeastern.taapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
